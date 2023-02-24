3feeb7b36396f85bde31ae4ab907a7439389b03b
Frosetrain <80436189+frosetrain@users.noreply.github.com>
2023-02-24 15:47:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ from pybricks.tools import wait, StopWatch
 hub = PrimeHub()
 
 left_motor = Motor(
-    Port.B,
+    Port.F,
     positive_direction=Direction.COUNTERCLOCKWISE,
     gears=None,
     reset_angle=True
 )
 right_motor = Motor(
-    Port.A,
+    Port.D,
     positive_direction=Direction.CLOCKWISE,
     gears=None,
     reset_angle=True
 )
 
-l_sensor = ColorSensor(Port.C)
-r_sensor = ColorSensor(Port.D)
+l_sensor = ColorSensor(Port.E)
+r_sensor = ColorSensor(Port.C)
 
 db = DriveBase(left_motor, right_motor, 88, 128)
     
@@ -36,4 +36,8 @@ db = DriveBase(left_motor, right_motor, 88, 128)
 while True:
     l_co = (l_sensor.reflection() - 7) / 55
     r_co = (r_sensor.reflection() - 8) / 58
-    db.drive((1 - abs(l_co - r_co)) * 400, (l_co - r_co) * 200)
+    #db.drive((1 - abs(l_co - r_co)) * 350, (l_co - r_co) * 275)
+    db.drive(300, (l_co - r_co) * 475)
+# 350, 250
+
+#Ratio of 7:5
