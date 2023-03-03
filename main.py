fd1715196969491bf0efd6c6bd6441eb9832a6d4
Frosetrain <80436189+frosetrain@users.noreply.github.com>
2023-03-03 16:13:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,26 +18,24 @@ right_motor = Motor(
     gears=None,
     reset_angle=True
 )
+claw_motor = Motor(
+    Port.A,
+    positive_direction=Direction.CLOCKWISE,
+    gears=None,
+    reset_angle=True
+)
 
 l_sensor = ColorSensor(Port.E)
 r_sensor = ColorSensor(Port.C)
 
 db = DriveBase(left_motor, right_motor, 88, 128)
-    
-"""
-    # l_co = (l_sensor.reflection() - 7)/55
-    # r_co = (r_sensor.reflection() - 8)/58;
-    # print(l_co, r_co)
-    # wait(50)
-    # MAX: 62, 66
-    # MIN: 7, 8
-"""
+
 
 while True:
-    l_co = (l_sensor.reflection() - 7) / 55
-    r_co = (r_sensor.reflection() - 8) / 58
-    #db.drive((1 - abs(l_co - r_co)) * 350, (l_co - r_co) * 275)
-    db.drive(300, (l_co - r_co) * 475)
-# 350, 250
-
-#Ratio of 7:5
+    #claw_motor.run_time(250, 500)
+    #claw_motor.run_time(-250, 500)
+    l_co = (l_sensor.reflection() - 5) / 50
+    r_co = (r_sensor.reflection() - 6) / 53
+    #db.drive((1 - abs(l_co - r_co)) * 350, (l_co - r_co) * 250)
+    # db.drive(250, (l_co - r_co) * 300)
+    # 350, 250 (7:5)
